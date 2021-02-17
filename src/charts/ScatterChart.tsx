8be281c9a910f6a8e7c76bf9703e03bd673dd3d7
Zander1983 <markkelly1983@yahoo.co.uk>
2021-02-17 16:02:40 +0000

    copy of abi's work

---
+++ src/charts/ScatterChart.tsx
@@ -0,0 +1,149 @@
+import React, { useEffect, useState,useRef } from 'react';
+import Chartjs from 'chart.js';
+import Scatter from './Scatter';
+
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+import { useParams } from 'react-router-dom';
+interface ParamTypes {
+    workspaceId: string;
+}
+interface graphInterface{
+    lableData:any;
+    graphData:any;
+    eventsData:any[];
+    id:string;
+    paramsData:any[];
+    onChangeEvent:any;
+}
+const ScatterChart = (props:graphInterface)=>{
+    const [yaxis,setYaxis] = useState(props.graphData['paramY']);
+    const [xaxis,setXaxis] = useState(props.graphData['paramX']);
+    const [Xlabel,setXLabel] = useState(props.graphData['paramXName']);
+    const [Ylabel,setYLabel] = useState(props.graphData['paramYName']);
+    const [graphData,setGraphData] = useState(props.graphData);
+    const {workspaceId} = useParams<ParamTypes>();
+
+    let newData:any[];
+    const getChartData = (x:number,y:number)=>{
+        return props.eventsData.map((res,i)=>{
+            return({x:res[x],y:res[y]})
+        })
+    }
+    newData = getChartData(xaxis,yaxis);
+    const [chartData,setChartData] = useState({
+        "datasets":[{
+            backgroundColor: "black",
+            pointBorderColor: "black",
+            pointBackgroundColor: "black","data":newData}]
+        });
+    useEffect(()=>{
+        newData = getChartData(xaxis,yaxis);
+        setChartData({
+            "datasets":[{
+                backgroundColor: "black",
+                pointBorderColor: "black",
+                pointBackgroundColor: "blue","data":newData}]
+            })
+            setXLabel(props.lableData[xaxis]);
+            setYLabel(props.lableData[yaxis]);
+    },[yaxis,xaxis])
+
+    const handleXaxisChange = (event:any) => {
+        const newAxis = event.target.value;
+        const newAxisLable = props.lableData[newAxis];
+        const newGraphData = {...graphData,paramX:newAxis,paramXName:newAxisLable}
+        setGraphData(newGraphData);
+        setXaxis(newAxis);
+        props.onChangeEvent({reqData:newGraphData,paramX:event.target.value,graphId:props.id});
+    };
+    const handleYaxisChange = (event:any) => {
+        const newAxis = event.target.value;
+        const newAxisLable = props.lableData[newAxis];
+        const newGraphData = {...graphData,paramY:newAxis,paramYName:newAxisLable}
+        setGraphData(newGraphData);
+        setYaxis(newAxis);
+        props.onChangeEvent({paramY:event.target.value,graphId:props.id});
+    };
+    return(
+        <>
+            <div className="mx-2 my-2" style={{position: "relative", height:'fit-content', width:`fit-content`,border:'1px solid black'}}>
+                <div style={{display:"flex",flexDirection:"column"}}>
+                    <div style={{display:"flex",flexDirection:"row"}}>
+                        <div className="mb-auto">
+                            <Select
+                                value={yaxis}
+                                onChange={handleYaxisChange}
+                                style={{marginBottom:"auto"}}
+                            >
+                                {
+                                    props.paramsData.map((data,index)=>{
+                                        return(
+                                            <MenuItem key={data.key} value={data.key}>{data.value}</MenuItem>
+                                        )
+                                    })
+                                }
+                            </Select>
+                        </div>
+                        <Scatter workspaceId={workspaceId} graphData={graphData} graphId={graphData._id} yaxis={graphData.paramY} xaxis={graphData.paramX} Xlabel={Xlabel} Ylabel={Ylabel} chartData={chartData}/>
+                        
+                    </div>
+                    <div className="ml-auto">
+                    <Select
+                        value={xaxis}
+                        onChange={handleXaxisChange}
+                        style={{marginLeft:"auto"}}
+                    >
+                        {
+                            props.paramsData.map((data,index)=>{
+                                return(
+                                    <MenuItem key={data.key} value={data.key}>{data.value}</MenuItem>
+                                )
+                            })
+                        }
+                    </Select>
+                    </div>
+                </div>
+            </div>
+            {/* <div style={{display:"flex",flexDirection:"column"}}>
+                <div className="mx-3 my-2">
+                    <div style={{display:"flex",flexDirection:"row"}}>
+                        <Select
+                            value={yaxis}
+                            onChange={handleYaxisChange}
+                            style={{marginBottom:"auto"}}
+                        >
+                            {
+                                props.paramsData.map((data,index)=>{
+                                    return(
+                                        <MenuItem key={data.key} value={data.key}>{data.value}</MenuItem>
+                                    )
+                                })
+                            }
+                        </Select>
+                        <div>
+                            <Scatter data={chartData} options={options} height={400} width={400} />
+                        </div>
+                    </div>
+                    <div style={{width:"100%",display:"flex"}}>
+                    <Select
+                        value={xaxis}
+                        onChange={handleXaxisChange}
+                        style={{marginLeft:"auto"}}
+                    >
+                        {
+                            props.paramsData.map((data,index)=>{
+                                return(
+                                    <MenuItem key={data.key} value={data.key}>{data.value}</MenuItem>
+                                )
+                            })
+                        }
+                    </Select>
+                </div>
+                </div>
+            </div> */}
+        </>
+    )
+}
+export default ScatterChart;
---
+++ src/sample_data/graphs.ts
@@ -0,0 +1,68 @@
+const graphsData = [
+    {
+        "paramX" : 0,
+        "paramY" : 1,
+        "paramXName" : "FSC-A",
+        "paramYName" : "SSC-A",
+        "xScale" : "lin",
+        "yScale" : "lin",
+        "isDotPlot" : true,
+        "selectedGate" : "All",
+        "graphName" : "graph1",
+        "index" : 1,
+        "width" : 270,
+        "height" : 270,
+        "_id" : "6022e70fa53f4f9a63367f73",
+        "fileIds" : [ ]
+    },
+    {
+        "paramX" : 4,
+        "paramY" : 1,
+        "paramXName" : "FSC-A",
+        "paramYName" : "SSC-A",
+        "xScale" : "lin",
+        "yScale" : "lin",
+        "isDotPlot" : true,
+        "selectedGate" : "All",
+        "width" : 270,
+        "height" : 270,
+        "graphName" : "graph2",
+        "index" : 2,
+        "_id" : "602436e1fb9530082a1a1a82",
+        "fileIds" : [ ]
+    },
+    {
+        "paramX" : 5,
+        "paramY" : 0,
+        "paramXName" : "FITC-A",
+        "paramYName" : "FSC-A",
+        "xScale" : "lin",
+        "yScale" : "lin",
+        "isDotPlot" : true,
+        "selectedGate" : "All",
+        "width" : 270,
+        "height" : 270,
+        "graphName" : "graph4",
+        "index" : 4,
+        "_id" : "602436f6fb9530082a1a1a84",
+        "fileIds" : [ ]
+    },
+    {
+        "paramX" : 1,
+        "paramY" : 0,
+        "paramXName" : "Time",
+        "paramYName" : "FSC-A",
+        "xScale" : "lin",
+        "yScale" : "lin",
+        "isDotPlot" : false,
+        "selectedGate" : "All",
+        "width" : 270,
+        "height" : 270,
+        "graphName" : "graph5",
+        "index" : 5,
+        "_id" : "602437e3fb9530082a1a1a85",
+        "fileIds" : [ ]
+    }
+]
+
+export default graphsData;
