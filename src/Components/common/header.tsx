6b26fe2269c21c064c858c5e5cdc67df8936451f
abhishek2524 <66551338+abhishek2524@users.noreply.github.com>
2021-02-24 16:54:26 +0530

    home page update

---
+++ src/Components/common/header.tsx
@@ -0,0 +1,56 @@
+import React, { useState,FC } from 'react';
+
+import { Anchor, Drawer, Button } from 'antd';
+
+const { Link } = Anchor;
+
+const AppHeader:FC = ()=>{
+    const [visible, setVisible] = useState(false);
+
+    const showDrawer = () => {
+      setVisible(true);
+    };
+  
+    const onClose = () => {
+      setVisible(false);
+    };
+    return (
+        <div className="container-fluid">
+          <div className="header">
+            <div className="logo">
+              <a href="#">Red Matter</a>
+            </div>
+            <div className="mobileHidden">
+              <Anchor>
+                  <Link href="#home" title="Home" />
+                  <Link href="#work" title="How it works" />
+                  <Link href="#help" title="Help" />
+                  <Link href="#contact" title="Contact" />
+                  <Link href="#blog" title="Blog" />
+              </Anchor>
+            </div>
+            <div className="mobileVisible">
+              <Button type="primary" onClick={showDrawer}>
+                <i className="fas fa-bars"></i>
+              </Button>
+              <Drawer
+                placement="left"
+                closable={false}
+                onClose={onClose}
+                visible={visible}
+              >
+                <Anchor>
+                  <Link href="#home" title="Home" />
+                  <Link href="#work" title="How it works" />
+                  <Link href="#help" title="Help" />
+                  <Link href="#contact" title="Contact" />
+                  <Link href="#blog" title="Blog" />
+                </Anchor>
+              </Drawer>
+            </div>
+          </div>
+        </div>
+      );
+}
+
+export default AppHeader;
