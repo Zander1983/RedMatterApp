3379065955145297493eaa0dbcce5c0863f5c78f
abhishek2524 <66551338+abhishek2524@users.noreply.github.com>
2021-03-11 23:10:16 +0530

    form update6

--- src/Components/home/questions.tsx
+++ src/Components/home/questions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {machinesData,fluorophoresData} from './quesData';
 import {
     message,
@@ -26,6 +26,7 @@ import { NavLink } from 'react-router-dom';
 
 const Questions = ()=>{
     const Quesform = ()=>{
+        const [sectionId,setSectionId] = useState(1);
         const deviceList = [
             {id:1,key:1,value:"ZE5 Cell Analyzer"}, {id:2,key:2,value:"S3e Cell Sorter"}
         ]
@@ -39,6 +40,7 @@ const Questions = ()=>{
             {id:1,key:1,value:"Single cells"}, {id:2,key:2,value:"Heterogenous population"}
         ]
         const onFinish = (values: any) => {
+            nextSection();
             message.success('Successfully Sent');
             console.log('Received values of form: ', values);
         };
@@ -59,24 +61,77 @@ const Questions = ()=>{
                 width:100
             }
         ]
+        const nextSection = ()=>{
+            setSectionId((curId:any) => curId+1);
+        }
+
+        const prevSection = ()=>{
+            setSectionId((curId:any) => curId-1);
+        }
+
+        const renderButton = ()=>{
+            if(sectionId>5){
+                return undefined;
+            }else{
+                return(
+                    <Row>
+                        <Col sm={24} md={24} xl={24}>
+                            <div className="btns">
+                                {sectionId > 1 && 
+                                    <Button onClick={prevSection} className="nextBtn">
+                                        Previous
+                                    </Button> 
+                                }
+                            
+                            { sectionId < 5 &&
+                                <Button onClick={nextSection} className="nextBtn">
+                                    Next
+                                </Button>
+                            }
+
+                            
+                            {  sectionId === 5 && <Form.Item>
+                                <Button htmlType="submit">
+                                    Submit
+                                </Button> 
+                            </Form.Item>}
+
+                            </div>
+                        </Col>
+                    </Row>
+                )
+            }
+        }
         return(
-            <div className="form-div">
-            <Form
+            <div className="main-form-div">
+                <div className="step-row" style={{width:'100%',height:"40px",margin:"0 auto",display:"flex",alignItems:"center",boxShadow:"0 -1px 5px -1px #000",position:"relative"}}>
+                    <div id="progress" style={{position:"absolute",height:"100%",width:`${100/6}%`}}></div>
+                    <div className={sectionId === 1?"step-col-active":"step-col"} style={{width:`${100/6}%`,textAlign:"center",color:"#333",position:"relative"}}><small>Step1</small></div>
+                    <div className={sectionId === 2?"step-col-active":"step-col"} style={{width:`${100/6}%`,textAlign:"center",color:"#333",position:"relative"}}><small>Step2</small></div>
+                    <div className={sectionId === 3?"step-col-active":"step-col"} style={{width:`${100/6}%`,textAlign:"center",color:"#333",position:"relative"}}><small>Step3</small></div>
+                    <div className={sectionId === 4?"step-col-active":"step-col"} style={{width:`${100/6}%`,textAlign:"center",color:"#333",position:"relative"}}><small>Step4</small></div>
+                    <div className={sectionId === 5?"step-col-active":"step-col"} style={{width:`${100/6}%`,textAlign:"center",color:"#333",position:"relative"}}><small>Step5</small></div>
+                    <div className={sectionId === 6?"step-col-active":"step-col"} style={{width:`${100/6}%`,textAlign:"center",color:"#333",position:"relative"}}><small>Step6</small></div>
+                </div>
+
+                <div className="form-div" >
+                <Form
                 name="questionForm"
                 onFinish={onFinish}
+                style={{display:"flex",background:'rgba(100, 120, 140, 0.99)',marginTop:"10%",position:"relative",flexDirection:"column"}}
             >
-
-                <Row>
-                    <Col sm={24} md={12} xl={12}>
+                {
+                    sectionId === 1 && 
+                    <section className="section1">
                         <Row>
                             <Col span={24}>
                                 <Form.Item name="device"
-                                    label="To optimize analysis please select your Device" style={{display:'block'}}
+                                    label={<label className="label">To optimize analysis please select your Device</label>} style={{display:'block'}}
                                     // rules={[{ required: true, message: 'Please select your Device!' }]}
                                 >
                                         <Select
                                             showSearch
-                                            style={{ width: "80%" }}
+                                            style={{ width: "100%" }}
                                             optionFilterProp="children"
                                             filterOption={(input:any, option:any) =>
                                                 option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
@@ -90,15 +145,20 @@ const Questions = ()=>{
                                 </Form.Item>                                
                             </Col>
                         </Row>
+                    </section>
+                }
+                {
+                    sectionId === 2 &&
+                    <section className="section1">
                         <Row>
                             <Col span={24}>
                                 <Form.Item name="cell_type"
                                     style={{display:'block'}}
-                                    label="What is the cell type are you measuring?"
+                                    label={<label className="label">What is the cell type are you measuring?</label>}
                                 // rules={[{ required: true, message: 'Please select Cell Type!' }]}
                                 >
                                     <Select 
-                                    style={{ width: "80%" }}
+                                    style={{ width: "100%" }}
                                     placeholder="Please select a Cell Type">
                                     {cellTypeList.map((data:any,index:number)=>{
                                         return <Option key={`celltype${index}`} value={data.key}>{data.value}</Option>
@@ -107,14 +167,20 @@ const Questions = ()=>{
                                 </Form.Item>
                             </Col>                    
                         </Row>
+                    </section>
+                }
+                
+                {
+                    sectionId === 3 &&
+                    <section className="section1">
                         <Row>
                             <Col span={24}>
                                 <Form.Item name="particles_size"
                                     style={{display:'block'}}
-                                    label="How big are the particles you are measuring?"
+                                    label={<label className="label">How big are the particles you are measuring?</label>}
                                 // rules={[{ required: true, message: 'Please select your Device!' }]}
                                 >
-                                    <Select style={{ width: "80%" }} placeholder="Please select a Particle Size">
+                                    <Select style={{ width: "100%" }} placeholder="Please select a Particle Size">
                                     {particlesSizeList.map((data:any,index:number)=>{
                                         return <Option key={`cellsize${index}`} value={data.key}>{data.value}</Option>
                                     })}
@@ -122,52 +188,74 @@ const Questions = ()=>{
                                 </Form.Item>
                             </Col>                    
                         </Row>
-                    </Col>
-                    <Col sm={24} md={12} xl={12}>
+                    </section>
+                }
+                {
+                    sectionId === 4 &&
+                    <section className="section1">
                         <Row>
                             <Col span={24}>
-                                <Form.Item style={{display:'block'}} name="description"
-                                label="Enter brief experiment description"
-                                // rules={[{ required: true, message: 'Please enter experiment description!' }]}
+                                <Form.Item style={{display:'block'}} name="fluorophores" 
+                                label={<label className="label">Select the fluorophores in your analysis</label>}
                                 >
-                                    <TextArea rows={10}/>
+                                    <Select
+                                        showSearch
+                                        style={{ width: "100%" }}
+                                        placeholder="Select the fluorophores in your analysis"
+                                        optionFilterProp="children"
+                                        filterOption={(input:any, option:any) =>
+                                            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                        }
+                                    >
+                                        {fluorophoresData.map((data:any,index:number)=>{
+                                            return <Option key={`device${index}`} value={data.key}>{data.value}</Option>
+                                        })}
+                                    </Select>
                                 </Form.Item>
                             </Col>
                         </Row>
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col sm={24} md={12} xl={12}>
-                        <Form.Item style={{display:'block'}} name="fluorophores" label="Select the fluorophores in your analysis">
-                            <Select
-                                showSearch
-                                style={{ width: "80%" }}
-                                placeholder="Select the fluorophores in your analysis"
-                                optionFilterProp="children"
-                                filterOption={(input:any, option:any) =>
-                                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                                }
-                            >
-                                {fluorophoresData.map((data:any,index:number)=>{
-                                    return <Option key={`device${index}`} value={data.key}>{data.value}</Option>
-                                })}
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    {/* <Col sm={24} md={12} xl={12}>
-                        <Table columns={columns}/>
-                    </Col> */}
-                </Row>
-
+                    </section>
+                }
+                {
+                    sectionId === 5 &&
+                    <section className="section1" style={{width:"80%"}}>
+                        <Row>
+                            <Col span={24}>
+                                <Form.Item style={{display:'block'}} name="description"
+                                label={<label className="label">Enter brief experiment description</label>}
+                                // rules={[{ required: true, message: 'Please enter experiment description!' }]}
+                                >
+                                    <TextArea rows={5}/>
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                    </section>
+                }
+                {
+                    sectionId === 6 &&
+                    <section className="final-section">
+                        <Row>
+                            <Col span={24}>
+                                <h1>Successfully Submitted</h1>
+                            </Col>
+                        </Row>
+                    </section>
+                }
+                <section>
+                    {
+                        renderButton()
+                    }
+                </section>
             </Form>
+                </div>
+            
             </div>
         )
     }
     return(
-        <div className="block" style={{background:'#ddf'}}>
-            <div className="container-fluid">
-                <h2 className="textCenter">Please Enter Following Details :</h2>
+        <div className="block" style={{background:'lightgrey'}}>
+            <div className="container-fluid" style={{height:"500px",width:"500px",position:"relative"}}>
+                {/* <h2 className="textCenter">Please Enter Following Details :</h2>*/}
                 <Quesform/>
             </div>
         </div>
