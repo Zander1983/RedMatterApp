f83d45c43806c592a8e19ad559917cba383c31cd
RenatoBrittoAraujo <renatobritto@protonmail.com>
2021-10-13 11:08:47 -0300

    Add handler for nul lgateID on provisory gate fetch

--- src/graph/utils/workspaceEventQueue.ts
+++ src/graph/utils/workspaceEventQueue.ts
@@ -1,32 +1,15 @@
-import { WorkspacesApiFetchParamCreator } from "api_calls/nodejsback";
-import axios from "axios";
-import userManager from "Components/users/userManager";
-import WorkspaceDispatch from "graph/workspaceRedux/workspaceDispatchers";
-import {
-  File,
-  FileID,
-  Gate,
-  GateID,
-  Plot,
-  PlotID,
-  Population,
-  PopulationID,
-  Workspace,
-  WorkspaceEvent,
-  WorkspaceEventGateNaming,
-} from "graph/resources/types";
+import { GateID, WorkspaceEventGateNaming } from "graph/resources/types";
 import { store } from "redux/store";
-import { dowloadAllFileEvents } from "services/FileService";
-import { snackbarService } from "uno-material-ui";
 
 export const getWorkspaceQueue = () => {
   return store.getState().workspaceEventQueue.queue;
 };
 
 export const checkIfGateNamingQueue = (gateId: GateID) => {
+  if (!gateId) return false;
   let workspaceQueue = getWorkspaceQueue();
   let events = workspaceQueue.filter(
-    (x: WorkspaceEventGateNaming) => x.gateID == gateId
+    (x: WorkspaceEventGateNaming) => x.gateID === gateId
   );
   return events.length > 0 ? true : false;
 };
