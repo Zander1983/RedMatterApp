e5c9db2f1caa44131dfde07f52c5b7bf0a26affa
RahulDevskamp <riyasaat@devskmap>
2021-10-26 21:28:37 +0600

    File Retry Download Added

--- src/services/FileService.tsx
+++ src/services/FileService.tsx
@@ -57,100 +57,115 @@ export const downloadFileEvent = async (
   workspaceIsShared: boolean,
   targetFiles: string | string[],
   experimentId: string,
-  showNotifications: boolean = true
+  showNotifications: boolean = true,
+  retry: number = 3
 ): Promise<FileID | FileID[]> => {
-  let notification: Notification;
-  if (showNotifications) {
-    notification = new Notification(
-      "Dowloading file" + (typeof targetFiles === "string" ? "" : "s")
-    );
-  }
-
-  let files: FileID[] = [];
-  if (typeof targetFiles === "string") {
-    files = [targetFiles];
-  } else {
-    files = targetFiles;
-  }
-
-  const workspace = getWorkspace();
-
-  for (const fileId of files) {
-    const fileQuery = workspace.files.filter((e) => e.id === fileId);
-    if (fileQuery.length > 1) {
-      throw Error("Multiple files with the same ID present in workspace");
-    }
-    if (fileQuery.length > 0 && fileQuery[0].downloaded) {
-      throw Error("File already downloaded");
-    }
-  }
-
-  let downloadingFiles: File[] = files.map((e) => getFile(e));
-  downloadingFiles.forEach((e) => {
-    e.downloading = true;
-    WorkspaceDispatch.UpdateFile(e);
-  });
-
-  let response;
-  let payload: {
-    experimentId: string;
-    fileIds: string[];
-    isShared?: boolean;
-    organisationId?: string;
-  } = {
-    experimentId: experimentId,
-    fileIds: files,
-  };
-
-  if (workspaceIsShared) {
-    payload = { ...payload, isShared: true };
-  } else {
-    payload = {
-      ...payload,
-      organisationId: userManager.getOrganiztionID(),
-    };
-  }
-
-  let token = null;
   try {
-    token = userManager.getToken();
-  } catch {}
-
-  let headers = {};
-  if (token) headers = { token };
-
-  response = await axios.post("/api/events", payload, {
-    headers,
-  });
-
-  response.data = response.data.map((e: any) => {
-    if (e.events.length > EVENTS_LIMIT) {
-      e.events = e.events.slice(0, EVENTS_LIMIT);
+    let notification: Notification;
+    if (showNotifications) {
+      notification = new Notification(
+        "Dowloading file" + (typeof targetFiles === "string" ? "" : "s")
+      );
     }
-    return e;
-  });
 
-  for (const file of response.data) {
-    let newFile = await createFile({
-      //@ts-ignore
-      requestData: file,
-      //@ts-ignore
-      id: file.id,
+    let files: FileID[] = [];
+    if (typeof targetFiles === "string") {
+      files = [targetFiles];
+    } else {
+      files = targetFiles;
+    }
+
+    const workspace = getWorkspace();
+
+    for (const fileId of files) {
+      const fileQuery = workspace.files.filter((e) => e.id === fileId);
+      if (fileQuery.length > 1) {
+        throw Error("Multiple files with the same ID present in workspace");
+      }
+      if (fileQuery.length > 0 && fileQuery[0].downloaded) {
+        throw Error("File already downloaded");
+      }
+    }
+
+    let downloadingFiles: File[] = files.map((e) => getFile(e));
+    downloadingFiles.forEach((e) => {
+      e.downloading = true;
+      WorkspaceDispatch.UpdateFile(e);
     });
-    //@ts-ignore
-    newFile = { ...newFile, ...getFile(file.id) };
-    newFile.downloaded = true;
-    newFile.downloading = false;
-    WorkspaceDispatch.UpdateFile(newFile);
-  }
-  if (showNotifications) {
-    notification.killNotification();
-  }
 
-  if (typeof targetFiles === "string") {
-    return targetFiles;
-  } else {
-    return files;
+    let response;
+    let payload: {
+      experimentId: string;
+      fileIds: string[];
+      isShared?: boolean;
+      organisationId?: string;
+    } = {
+      experimentId: experimentId,
+      fileIds: files,
+    };
+
+    if (workspaceIsShared) {
+      payload = { ...payload, isShared: true };
+    } else {
+      payload = {
+        ...payload,
+        organisationId: userManager.getOrganiztionID(),
+      };
+    }
+
+    let token = null;
+    try {
+      token = userManager.getToken();
+    } catch {}
+
+    let headers = {};
+    if (token) headers = { token };
+
+    response = await axios.post("/api/events", payload, {
+      headers,
+    });
+
+    response.data = response.data.map((e: any) => {
+      if (e.events.length > EVENTS_LIMIT) {
+        e.events = e.events.slice(0, EVENTS_LIMIT);
+      }
+      return e;
+    });
+
+    for (const file of response.data) {
+      let newFile = await createFile({
+        //@ts-ignore
+        requestData: file,
+        //@ts-ignore
+        id: file.id,
+      });
+      //@ts-ignore
+      newFile = { ...newFile, ...getFile(file.id) };
+      newFile.downloaded = true;
+      newFile.downloading = false;
+      WorkspaceDispatch.UpdateFile(newFile);
+    }
+    if (showNotifications) {
+      notification.killNotification();
+    }
+
+    if (typeof targetFiles === "string") {
+      return targetFiles;
+    } else {
+      return files;
+    }
+  } catch (err) {
+    if (retry > 0) {
+      downloadFileEvent(
+        workspaceIsShared,
+        targetFiles,
+        experimentId,
+        (showNotifications = true),
+        retry - 1
+      );
+    } else {
+      throw "File was not downloaded";
+    }
   }
 };
 
