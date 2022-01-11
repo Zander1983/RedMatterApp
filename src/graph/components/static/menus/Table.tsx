667371698cbcf46dcbf059bf20584ea62305be9d
RahulDevskamp <riyasaat@devskmap>
2022-01-11 12:41:05 +0600

    asc and dsc sorting added to the table

--- src/graph/components/static/menus/Table.tsx
+++ src/graph/components/static/menus/Table.tsx
@@ -11,6 +11,9 @@ import { Workspace } from "graph/resources/types";
 import PlotStats from "graph/utils/stats";
 import { useEffect, useState } from "react";
 
+import upArrow from "./../../../../assets/images/up_arrow.png";
+import downArrow from "./../../../../assets/images/down_arrow.png";
+
 const useStyles = makeStyles((theme) => ({
   container: {
     position: "absolute",
@@ -36,6 +39,12 @@ const useStyles = makeStyles((theme) => ({
     height: "15vh",
     transition: "height 1s ease",
   },
+  arrow: {
+    height: 15,
+    width: 10,
+    marginLeft: 5,
+    cursor: "pointer",
+  },
 }));
 
 interface TableProps {
@@ -59,7 +68,7 @@ const PlotTable = ({ workspace }: TableProps) => {
   let gateLength: number = 1;
   const fillUpRows = () => {
     for (let i = 0; i < workspace.files.length; i++) {
-      const raw = [];
+      const raw = [fileNames[i]];
       for (let j = 0; j < statistics.length; j += workspace.files.length) {
         if (statistics[i + j]?.gatedFilePopulationPercentage) {
           raw.push(statistics[i + j]?.gatedFilePopulationPercentage);
@@ -87,17 +96,65 @@ const PlotTable = ({ workspace }: TableProps) => {
     setStatistics(stats);
   };
 
+  const sortByColumn = (colIndex: number, type: string) => {
+    let array = data;
+
+    // filtering out the neumeric values from the string
+    array = array.map((arr) => {
+      return arr.map((value: any, index: number) => {
+        if (index) {
+          return parseFloat(value.match(/[-+]?([0-9]*\.[0-9]+|[0-9]+)/));
+        } else {
+          return value;
+        }
+      });
+    });
+
+    array.sort(sortFunction);
+
+    // this is the sort function
+    function sortFunction(a: string[], b: string[]) {
+      if (a[colIndex] === b[colIndex]) {
+        return 0;
+      } else {
+        if (type === "asc") {
+          return a[colIndex] < b[colIndex] ? -1 : 1;
+        } else if (type === "dsc") {
+          return a[colIndex] > b[colIndex] ? -1 : 1;
+        }
+      }
+    }
+
+    // converting the neumeric values to suitable string format
+    array = array.map((arr) => {
+      return arr.map((value: number, index: number) => {
+        if (index) {
+          if (value === 1) {
+            return `< ${value}%`;
+          } else {
+            return `${value} %`;
+          }
+        } else {
+          return value;
+        }
+      });
+    });
+
+    setData(array);
+  };
+
   useEffect(() => {
     gateLength = workspace.gates.length;
     setFileNames(workspace.files.map((file) => file.name));
-    // works with plot (takes so much time)
-    // workspace.plots.map((plot, id) => {
-    //   id && stats.push(statsProvider.getPlotStats(plot, 1, 1));
-    // });
 
     // works with files and events (takes less time)
     updateStats();
   }, [workspace]);
+
+  useEffect(() => {
+    workspace.files.map((file) => setData((prev) => [...prev, [file.name]]));
+  }, [workspace.files.length]);
+  console.log(data);
 
   useEffect(() => {
     setHeaders([
@@ -116,9 +173,29 @@ const PlotTable = ({ workspace }: TableProps) => {
       <Table style={{ overflowY: "scroll" }}>
         <TableHead>
           <TableRow>
-            {headers.map((values) => (
+            {headers.map((values, index) => (
               <TableCell className={classes.tableCell} key={values}>
                 {values}
+                {index !== 0 && index !== headers.length - 1 && (
+                  <>
+                    <img
+                      onClick={() => {
+                        sortByColumn(index, "asc");
+                      }}
+                      src={downArrow}
+                      alt="down-arrow"
+                      className={classes.arrow}
+                    />
+                    <img
+                      onClick={() => {
+                        sortByColumn(index, "dsc");
+                      }}
+                      src={upArrow}
+                      alt="up-arrow"
+                      className={classes.arrow}
+                    />
+                  </>
+                )}
               </TableCell>
             ))}
           </TableRow>
@@ -127,9 +204,6 @@ const PlotTable = ({ workspace }: TableProps) => {
           {workspace?.files?.map((files, i) => (
             <>
               <TableRow key={i}>
-                <TableCell className={classes.tableCell}>
-                  {fileNames[i]}
-                </TableCell>
                 {headers.length > 1 &&
                   data.length > 0 &&
                   data[i]?.map((value: any, index: any) => (
