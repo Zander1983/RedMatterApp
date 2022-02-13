4129fdc9d0c7e97b5da93a244ee0969df061a759
Eyasin-PC\MD Eyasin <mohammadeyasin@gmail.com>
2022-02-13 23:59:13 +0600

    improved plot row component

--- src/graph/components/plots/PlotRowComponent.tsx
+++ src/graph/components/plots/PlotRowComponent.tsx
@@ -1,170 +1,30 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import {
     File,
-    Gate,
     Plot,
     PlotID,
-    PlotSpecificWorkspaceData,
     Population,
-    Workspace as WorkspaceType,
     Workspace,
     PlotsRerender,
 } from "../../resources/types";
 
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell/TableCell";
-import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
-import { standardGridPlotItem } from "../workspaces/PlotController";
-import PlotComponent from "./PlotComponent";
-import { makeStyles } from "@material-ui/core";
 import {
-    getFile,
-    getGate,
-    getPopulation,
-    getWorkspace,
+    getFile
 } from "../../utils/workspace";
 //@ts-ignore
-import { Responsive, WidthProvider } from "react-grid-layout";
 import * as PlotResource from "graph/resources/plots";
 import * as PopulationResource from "graph/resources/populations";
-import _ from "lodash";
 import PlotStateComponent from './PlotStateComponent';
+import PlotDataComponent from './PlotDataComponent';
 
 import WorkspaceDispatch from "graph/workspaceRedux/workspaceDispatchers";
 import EventQueueDispatch from "graph/workspaceRedux/eventQueueDispatchers";
-import PlotStats from "graph/utils/stats";
-const statsProvider = new PlotStats();
+
 
 interface PlotsAndFiles {
     plot: Plot;
     file: File;
 }
-
-const useStyles = makeStyles((theme) => ({
-    container: {
-        backgroundColor: "#FAFAFA",
-        width: "100%",
-        color: "#333",
-        padding: 20,
-    },
-    tableCell: {
-        width: "auto",
-        color: "#333",
-        border: "1px #333 solid",
-        textAlign: "center",
-        padding: "5px !important",
-    },
-    view: {
-        border: "none",
-        cursor: "pointer",
-        color: "white",
-        fontWeight: 500,
-        padding: "2px 5px",
-        background: "#333333",
-        minWidth: 90,
-        margin: "0px 8px",
-        borderRadius: 5,
-    },
-    arrow: {
-        height: 15,
-        width: 10,
-        marginLeft: 5,
-        cursor: "pointer",
-    },
-    delete: {
-        height: 15,
-        width: 15,
-        marginLeft: 10,
-        cursor: "pointer",
-    },
-    itemOuterDiv: {
-        flex: 1,
-        border: "solid 0.5px #bbb",
-        boxShadow: "1px 3px 4px #bbd",
-        borderRadius: 5,
-        paddingBottom: "2rem",
-        minWidth: 370,
-        backgroundColor: "rgb(238, 238, 255)",
-    },
-    itemInnerDiv: {
-        width: "100%",
-        height: "100%",
-    },
-    show: {
-        opacity: 1,
-        display: "block",
-    },
-    hide: {
-        opacity: 0,
-        display: "none",
-        transition: "opacity 1s ease-out",
-    },
-    responsiveContainer: {
-        marginTop: 3,
-        marginBottom: 10,
-    },
-    loaderContainerStyle: {
-        position: "relative",
-        left: "45vw",
-        height: 419,
-    },
-    loader: {
-        width: 50,
-        height: 50,
-    },
-}));
-const ResponsiveGridLayout = WidthProvider(Responsive);
-
-export const setCanvasSize = (
-    save: boolean = false,
-    isAsync: boolean = false
-) => {
-    const plots = getWorkspace().plots;
-    const updateList: Plot[] = [];
-    for (let plot of plots) {
-        let id = `canvas-${plot.id}`;
-        let displayRef = `display-ref-${plot.id}`;
-        let barRef = `bar-ref-${plot.id}`;
-
-        let docIdRef = document.getElementById(id);
-        let docDisplayRef: any = document.getElementById(displayRef);
-        let docBarRef: any = document.getElementById(barRef);
-
-        if (docBarRef && docDisplayRef && docIdRef) {
-            let width = docDisplayRef.offsetWidth - 50;
-            let height = docDisplayRef.offsetHeight - docBarRef.offsetHeight - 40;
-            plot.plotHeight = height;
-            plot.plotWidth = width;
-
-            docIdRef.setAttribute("style", `width:${width}px;height:${height}px;`);
-            updateList.push(plot);
-        }
-    }
-    if (save && plots.length > 0) {
-        if (isAsync)
-            _.debounce(() => WorkspaceDispatch.UpdatePlots(updateList), 100);
-        else WorkspaceDispatch.UpdatePlots(updateList); //setTimeout( () => , 10);
-    }
-};
-
-export const MINW = 9;
-export const MINH = 10;
-
-export const resetPlotSizes = (id?: string) => {
-    let tPlots = getWorkspace().plots.map((e) => e.id);
-    if (id) tPlots = [id];
-    for (const id of tPlots) {
-        let docIdRef = document.getElementById(`canvas-${id}`);
-        let docDisplayRef: any = document.getElementById(`display-ref-${id}`);
-        let docBarRef: any = document.getElementById(`bar-ref-${id}`);
-
-        if (docBarRef && docDisplayRef && docIdRef) {
-            let width = docDisplayRef.offsetWidth - 55;
-            let height = docDisplayRef.offsetHeight - docBarRef.offsetHeight - 40;
-            docIdRef.setAttribute("style", `width:${width}px;height:${height}px;`);
-        }
-    }
-};
 
 interface Props {
     sharedWorkspace: boolean;
@@ -176,11 +36,12 @@ interface Props {
     // arrowFunc: Function;
     file: File;
     headers: string[];
-    data: any[];
+    // data: any[];
     openFiles: string[];
     setOpenFiles: React.Dispatch<React.SetStateAction<string[]>>;
 }
-const PlotRowComponent = ({workspace, data,
+
+const PlotRowComponent = ({workspace,
                               sharedWorkspace,
                               customPlotRerender,
                               experimentId,
@@ -190,10 +51,6 @@ const PlotRowComponent = ({workspace, data,
                               openFiles,
                               setOpenFiles,
                           }: Props) => {
-    const classes = useStyles();
-
-    // console.log("Row: " + file.id);
-    // const [data, setData] = useState([]);
 
     const generatePlots = (file: File) => {
         if (file.view) {
@@ -297,77 +154,6 @@ const PlotRowComponent = ({workspace, data,
         }
     };
 
-    const getPlotRelevantResources = (plot: Plot) => {
-        const population = getPopulation(plot.population);
-        const file = getFile(population.file);
-        const gates: Gate[] = [
-            ...plot.gates.map((e) => getGate(e)).filter((x) => x),
-            ...population.gates.map((e) => getGate(e.gate)),
-        ];
-        const workspaceForPlot: PlotSpecificWorkspaceData = {
-            file,
-            gates,
-            plot,
-            population,
-            key: plot.id,
-        };
-        return workspaceForPlot;
-    };
-
-    const raws: any[] = [];
-
-    const fillUpRows = (statistics: any[]) => {
-        for (let i = 0; i < workspace.files.length; i++) {
-            let raw = [workspace.files[i].id];
-            for (let j = 0; j < statistics.length; j += workspace.files.length) {
-                if (statistics[i + j]?.gatedFilePopulationPercentage) {
-                    raw.push(statistics[i + j]?.gatedFilePopulationPercentage);
-                }
-            }
-            raw.push(workspace.files[i].view ? "Close" : "View");
-
-            // Removing unnecessary extra values
-            if (raw.length < workspace.gates.length + 2) {
-                raw = [
-                    raw[0],
-                    ...raw.slice(1, workspace.gates.length),
-                    raw[raw.length - 1] === "View" || "Close"
-                        ? "--"
-                        : raw[raw.length - 2],
-                    raw[raw.length - 1],
-                ];
-            } else {
-                raw = [
-                    raw[0],
-                    ...raw.slice(1, 1 + workspace.gates.length),
-                    raw[raw.length - 1],
-                ];
-            }
-
-            if (!raw.includes(undefined)) {
-                raws.push(raw);
-            }
-        }
-        // setData(raws);
-    };
-
-    const updateStats = () => {
-        let stats: any[] = [];
-        workspace.populations.map((population) => {
-            if (
-                population.gates.length > 0 &&
-                workspace.selectedFile === population.file
-            ) {
-                // workspace.files.map((file) => {
-                    if (file.downloaded && !file.downloading) {
-                        stats.push(statsProvider.getPlotStatsWithFiles(file, population));
-                    }
-                // });
-            }
-        });
-        fillUpRows(stats);
-    };
-
     useEffect(() => {
         if (file.id === workspace.selectedFile) {
             updatePlot();
@@ -376,9 +162,9 @@ const PlotRowComponent = ({workspace, data,
     }, []);
 
     // useEffect(() => {
-    //     console.log(workspace.files);
-    //     console.log(workspace.populations);
-    //     updateStats();
+    //     // console.log(workspace.files);
+    //     // console.log(workspace.populations);
+    //     //updateStats();
     // }, [workspace]);
 
     const onClick = () => {
@@ -396,7 +182,7 @@ const PlotRowComponent = ({workspace, data,
                 updatePlot();
                 setTimeout(() => {
                     generatePlots(file);
-                }, 500);
+                }, 0);
             }
         }
     };
@@ -404,87 +190,15 @@ const PlotRowComponent = ({workspace, data,
     const plotData = () => {
         //console.log("===== Plot Render =====");
         return(
-            <TableRow
-                className={
-                    file.id === workspace.selectedFile
-                        ? classes.show
-                        : openFiles.includes(file.id)
-                        ? classes.show
-                        : classes.hide
-                }>
-                {getTableRowPlots(file).length === 0 ? (
-                    <TableCell
-                        colSpan={headers.length}
-                        className={classes.loaderContainerStyle}
-                    >
-                        <CircularProgress className={classes.loader} />
-                    </TableCell>
-                ) : (
-                    <TableCell colSpan={headers.length}>
-                        <div
-                            className={classes.responsiveContainer}
-                            style={{
-                                opacity: file.view ? 1 : 0,
-                                transition: `all ${
-                                    workspace.files.length < 30
-                                        ? 1
-                                        : workspace.files.length < 60
-                                        ? 2
-                                        : 3
-                                    }s`,
-                            }}
-                        >
-                            <ResponsiveGridLayout
-                                className="layout"
-                                breakpoints={{ lg: 1200 }}
-                                cols={{ lg: 36 }}
-                                rows={{ lg: 30 }}
-                                rowHeight={30}
-                                compactType={null}
-                                isDraggable={workspace.editWorkspace}
-                                isResizable={false}
-                            >
-                                {
-                                    //@ts-ignore
-                                    getTableRowPlots(file).map(({ plot, file: PlotFile }, i) => {
-                                        if (PlotFile.id === file.id) {
-                                            return (
-                                                <div
-                                                    key={plot.id}
-                                                    className={classes.itemOuterDiv}
-                                                    data-grid={standardGridPlotItem(
-                                                        i,
-                                                        plot,
-                                                        workspace.plots,
-                                                        workspace.editWorkspace
-                                                    )}
-                                                    id={`workspace-outter-${plot.id}`}
-                                                >
-                                                    <div id="inner" className={classes.itemInnerDiv}>
-                                                        <PlotComponent
-                                                            plotRelevantResources={getPlotRelevantResources(
-                                                                plot
-                                                            )}
-                                                            sharedWorkspace={sharedWorkspace}
-                                                            editWorkspace={workspace.editWorkspace}
-                                                            workspaceLoading={workspaceLoading}
-                                                            customPlotRerender={customPlotRerender}
-                                                            experimentId={experimentId}
-                                                            fileName={file.name}
-                                                        />
-                                                    </div>
-                                                </div>
-                                            );
-                                        }
-                                    })
-                                }
-                            </ResponsiveGridLayout>
-                        </div>
-                    </TableCell>
-                )}
-            </TableRow>
+            <PlotDataComponent
+                sharedWorkspace={sharedWorkspace}
+                experimentId={experimentId}
+                workspace={workspace}
+                workspaceLoading={workspaceLoading}
+                customPlotRerender={customPlotRerender}
+                file={file} headers={headers} openFiles={openFiles} />
         );
-    }
+    };
 
     return (
         <>
@@ -492,8 +206,9 @@ const PlotRowComponent = ({workspace, data,
                 workspace={workspace}
                 file={file}
                 headers={headers}
-                data={data} openFiles={openFiles}
-                setOpenFiles={setOpenFiles}
+                // data={data[0]}
+                openFiles={openFiles}
+                // setOpenFiles={setOpenFiles}
                 onRowClick={onClick} />
             {file.view && plotData()}
 
