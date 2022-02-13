81c751633fbfd3546498c92192eea42eb13b0756
Eyasin-PC\MD Eyasin <mohammadeyasin@gmail.com>
2022-02-13 23:59:33 +0600

    improved plot state component

--- src/graph/components/plots/PlotStateComponent.tsx
+++ src/graph/components/plots/PlotStateComponent.tsx
@@ -7,7 +7,8 @@ import {
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell/TableCell";
 import { makeStyles } from "@material-ui/core";
-
+import PlotStats from "graph/utils/stats";
+const statsProvider = new PlotStats();
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -89,53 +90,103 @@ interface Props {
     onRowClick: Function;
     file: File;
     headers: string[];
-    data: any[];
+    // data: any[];
     openFiles: string[];
-    setOpenFiles: React.Dispatch<React.SetStateAction<string[]>>;
+    // setOpenFiles: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
-const PlotStateComponent = ({workspace,
-                              file,
-                              headers,
-                              data,
-                              openFiles, onRowClick
-                          }: Props) => {
+const PlotStateComponent = ({workspace, file, headers, openFiles, onRowClick}: Props) => {
+
     const classes = useStyles();
 
-    // useEffect(() => {
-    //     console.log("=== Plot Row State: " + file.id);
-    //     console.log(workspace.files);
-    //     console.log(workspace.populations);
-    // },[workspace]);
+    const [data, setData] = useState([]);
 
-    return (
-        <TableRow>
-            {headers && data !== undefined && headers.length > 1 && data.length > 0 &&
-            data?.map((value: any, i: any) => i === 0 ? (
-                <TableCell className={`${classes.tableCell}`} key={"content-" + value + i}>
-                    {workspace.files?.find((f) => f.id === value)?.name}
-                </TableCell>
-            ) : i !== data.length - 1 ? (
-                <TableCell className={classes.tableCell} key={"content-" + value + i}>
-                    {value || "NA"}
-                </TableCell>
-            ) : (
-                <TableCell
-                    className={`${classes.tableCell}`}
-                    key={"content-" + value + i}
-                    onClick={() => {onRowClick()}}>
-                    <button className={classes.view}
-                            style={{cursor: file.id === workspace.selectedFile ? "default" : "pointer",
-                                backgroundColor: file.id === workspace.selectedFile ? "#FAFAFA" : "#333",
-                                color: file.id === workspace.selectedFile ? "black" : "whilte"}}
-                            disabled={file.id === workspace.selectedFile}>
-                        {file.id === workspace.selectedFile ? "Selected File" : openFiles.includes(file.id) ? "Close": "View Plots"}
-                    </button>
-                </TableCell>
-            )
-            )}
-        </TableRow>
-    );
+    const raws: any[] = [];
+
+    const fillUpRows = (statistics: any[]) => {
+        // for (let i = 0; i < workspace.files.length; i++) {
+        let raw = [file.id];
+        for (let j = 0; j < statistics.length; j += workspace.files.length) {
+            if (statistics[j]?.gatedFilePopulationPercentage) {
+                raw.push(statistics[j]?.gatedFilePopulationPercentage);
+            }
+        }
+        raw.push(file.view ? "Close" : "View");
+
+        // Removing unnecessary extra values
+        if (raw.length < workspace.gates.length + 2) {
+            raw = [
+                raw[0],
+                ...raw.slice(1, workspace.gates.length),
+                raw[raw.length - 1] === "View" || "Close"
+                    ? "--"
+                    : raw[raw.length - 2],
+                raw[raw.length - 1],
+            ];
+        } else {
+            raw = [
+                raw[0],
+                ...raw.slice(1, 1 + workspace.gates.length),
+                raw[raw.length - 1],
+            ];
+        }
+
+        if (!raw.includes(undefined)) {
+            raws.push(raw);
+        }
+        // }
+        setData(raws[0]);
+    };
+
+    const updateStats = () => {
+        let stats: any[] = [];
+        workspace.populations.map((population) => {
+            if (
+                population.gates.length > 0 &&
+                workspace.selectedFile === population.file
+            ) {
+                // workspace.files.map((file) => {
+                if (file.downloaded && !file.downloading) {
+                    stats.push(statsProvider.getPlotStatsWithFiles(file, population));
+                }
+                //});
+            }
+        });
+        fillUpRows(stats);
+    };
+
+    useEffect(() => {
+        updateStats();
+    }, [workspace]);
+
+     return (
+         <TableRow>
+             {headers && data !== undefined && headers.length > 1 && data.length > 0 &&
+             data?.map((value: any, i: any) => i === 0 ? (
+                     <TableCell className={`${classes.tableCell}`} key={"content-" + value + i}>
+                         {workspace.files?.find((f) => f.id === value)?.name}
+                     </TableCell>
+                 ) : i !== data.length - 1 ? (
+                     <TableCell className={classes.tableCell} key={"content-" + value + i}>
+                         {value || "NA"}
+                     </TableCell>
+                 ) : (
+                     <TableCell
+                         className={`${classes.tableCell}`}
+                         key={"content-" + value + i}
+                         onClick={() => {onRowClick()}}>
+                         <button className={classes.view}
+                                 style={{cursor: file.id === workspace.selectedFile ? "default" : "pointer",
+                                     backgroundColor: file.id === workspace.selectedFile ? "#FAFAFA" : "#333",
+                                     color: file.id === workspace.selectedFile ? "black" : "whilte"}}
+                                 disabled={file.id === workspace.selectedFile}>
+                             {file.id === workspace.selectedFile ? "Selected File" : openFiles.includes(file.id) ? "Close": "View Plots"}
+                         </button>
+                     </TableCell>
+                 )
+             )}
+         </TableRow>
+     )
 };
 
 export default PlotStateComponent;
