1238759f42444cd4fbca7168b3b9149a4c157bf2
Keshav Agrawal <keshavagrawal21@gmail.com>
2022-04-05 14:11:35 +0530

    fixes

--- src/graph/mark-app/GraphPlotController.tsx
+++ src/graph/mark-app/GraphPlotController.tsx
@@ -438,7 +438,11 @@ class NewPlotController extends React.Component<PlotControllerProps, IState> {
       (x: any) => x.fileId == fileId
     );
 
-    let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
 
     if (!newWorkspaceState.files[fileId]) {
       newWorkspaceState.files[fileId] = { plots: foundEnrichedFile.plots };
