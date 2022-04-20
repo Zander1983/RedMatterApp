40c22f41758276b2c49c347baf9abeeabbd739b6
RiyasaatAhmed <rrahul171089@bscse.uiu.ac.bd>
2022-04-20 19:07:27 +0600

    UI changes for more than 4 plots

--- src/graph/mark-app/Table.js
+++ src/graph/mark-app/Table.js
@@ -98,8 +98,8 @@ function Table(props) {
         ></div>
       </div>
       <table className="workspace">
-        <tbody style={{ width: "100%" }}>
-          <tr>
+        <tbody style={{ maxWidth: "100%" }}>
+          <tr style={{ display: "flex", flexWrap: "wrap" }}>
             {controlEnrichedFile?.plots?.map((plot, plotIindex) => {
               return (
                 <th key={`td-${plotIindex}`}>
@@ -257,6 +257,11 @@ function Table(props) {
           <tr
             style={{
               border: "1px solid gray",
+              margin: 1,
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              marginBottom: 5,
             }}
           >
             {controlEnrichedFile.plots.map((plot, plotIindex) => {
@@ -266,9 +271,10 @@ function Table(props) {
                   style={{
                     textAlign: "center",
                     fontWeight: "bold",
-                    minWidth: 275,
+                    minWidth: plot.width + 170,
                     padding: 5,
-                    borderInline: "1px solid gray",
+                    margin: 0.5,
+                    border: "1px solid gray",
                   }}
                 >
                   <div
@@ -329,23 +335,28 @@ function Table(props) {
               <tr
                 key={`tr-${fileIndex}`}
                 style={{
-                  // border: editedFileIds.includes(enrichedFile.fileId)
-                  //   ? "4px solid #FCBA05"
-                  //   : "1px solid gray",
                   border: "1px solid gray",
+                  margin: 1,
+                  display: "flex",
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                  marginBottom: 5,
                 }}
               >
                 {enrichedFile.plots.map((plot, plotIindex) => {
                   return (
                     <td
                       key={`td-${plotIindex + 1}`}
-                      style={{ padding: 5, borderInline: "1px solid gray" }}
+                      style={{
+                        margin: 0.5,
+                        border: "1px solid gray",
+                        minWidth: plot.width + 170,
+                      }}
                     >
                       <div
                         style={{
                           textAlign: "center",
                           fontWeight: "bold",
-                          minWidth: 275,
                         }}
                       >
                         <Tooltip
