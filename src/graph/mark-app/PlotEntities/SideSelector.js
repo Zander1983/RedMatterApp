c0cf6442465da04572c227e2d0fe31df556da07b
Keshav Agrawal <keshavagrawal21@gmail.com>
2022-04-22 11:30:15 +0530

    axis overlap fix

--- src/graph/mark-app/PlotEntities/SideSelector.js
+++ src/graph/mark-app/PlotEntities/SideSelector.js
@@ -55,7 +55,7 @@ function SideSelector(props) {
             <Select
               disableUnderline
               style={{
-                width: props.plot.width * 0.6,
+                width: props.plot.height * 0.6,
                 textAlign: "center",
                 flex: "1 1 auto",
                 fontSize: 12,
@@ -92,7 +92,7 @@ function SideSelector(props) {
           <Select
             disableUnderline
             style={{
-              width: props.plot.width * 0.35,
+              width: props.plot.height * 0.35,
               textAlign: "center",
               flex: "1 1 auto",
               fontSize: 12,
