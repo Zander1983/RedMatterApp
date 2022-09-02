da1a19ba45cdafb25e6b7665a629a92bae30a13b
Zander1983 <markkelly1983@yahoo.co.uk>
2022-09-02 11:54:51 +0100

    bump

--- src/Components/common/Header.tsx
+++ src/Components/common/Header.tsx
@@ -97,7 +97,7 @@ const AppHeader = (props: any) => {
                     fontSize: 15,
                   }}
                 >
-                  {"v5.0.3"}
+                  {"v5.0.4"}
                 </b>
               </NavLink>
             </Typography>
