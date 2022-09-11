35dd7ffcf2d97eb358e403660aa21c82229e5f1b
Zander1983 <markkelly1983@yahoo.co.uk>
2022-09-11 16:13:15 +0100

    removed control file

--- src/Components/common/Header.tsx
+++ src/Components/common/Header.tsx
@@ -97,7 +97,7 @@ const AppHeader = (props: any) => {
                     fontSize: 15,
                   }}
                 >
-                  {"v5.0.15"}
+                  {"v5.0.16"}
                 </b>
               </NavLink>
             </Typography>
